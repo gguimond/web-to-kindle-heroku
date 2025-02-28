c1e8f956679e7fadc3680dc96c73244a4be57255
Guillaume Guimond <guillaume.guimond@orange.com>
2025-02-28 18:13:43 +0100

    updated weather url and parsing

--- index.js
+++ index.js
@@ -11,53 +11,46 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', async (req, res) => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 600, height: 800 });
-    await page.goto(process.env.SCREENSHOT_URL || 'https://darksky.net/details/40.7127,-74.0059/2021-1-6/us12/en');
-    await page.evaluate(() => {
-      if(document.querySelector("#banner")){
-        document.querySelector("#banner").remove()
-      }
-      if(document.querySelector("section:nth-child(1)")){
-        document.querySelector("section:nth-child(1)").remove()
-      }
-      if(document.querySelector("#header")){
-        document.querySelector("#header").remove()
-      }
-      if(document.querySelector("#map-container")){
-        document.querySelector("#map-container").remove()
-      }
-      document.querySelectorAll("#currentDetails > div").forEach(function(item) {
-        item.style.margin = '0'
+    try{
+      const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+      const page = await browser.newPage();
+      await page.setViewport({ width: 600, height: 800 });
+      await page.goto(process.env.SCREENSHOT_URL || 'https://www.lameteoagricole.net/meteo-heure-par-heure/Caen-14000-j1.html');
+      await page.evaluate(() => {
+        if(document.querySelector("#banner")){
+          document.querySelector("#banner").remove()
+        }
+        if(document.querySelector(".frame-content")){
+          document.querySelector(".frame-content > div:nth-child(1)").style.background = "none"
+        }
+        const t = document.querySelector(".bootstrap-table")
+        if(t){
+          document.body.appendChild(t)
+        }
+        for (const child of document.body.children) {
+          if(child !== t){
+            document.body.removeChild(child)
+          }
+        }
       });
-      document.querySelectorAll("#currentDetails .label").forEach(function(item) {
-        item.style.margin = '0'
+      await page.screenshot({
+        path: '/tmp/screenshot.png',
       });
-      if(document.querySelector("#week")){
-        document.querySelector("#week").style.margin = '-30px 0 0 0'
-      }
-      if(document.querySelector("#week > .summary")){
-        document.querySelector("#week > .summary").style.margin = '5px'
-      }
-      document.querySelectorAll(".day").forEach(function(item) {
-        item.style.margin = '0 auto 12px'
+
+      await browser.close();
+
+      await convert('/tmp/screenshot.png');
+      screenshot = fs.readFileSync('/tmp/screenshot.png');
+
+      res.writeHead(200, {
+        'Content-Type': 'image/png',
+        'Content-Length': screenshot.length,
       });
-    });
-    await page.screenshot({
-      path: '/tmp/screenshot.png',
-    });
-
-    await browser.close();
-
-    await convert('/tmp/screenshot.png');
-    screenshot = fs.readFileSync('/tmp/screenshot.png');
-
-    res.writeHead(200, {
-      'Content-Type': 'image/png',
-      'Content-Length': screenshot.length,
-    });
-    return res.end(screenshot);
+      return res.end(screenshot);
+    }
+    catch(e){
+      console.log(e)
+    }
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
